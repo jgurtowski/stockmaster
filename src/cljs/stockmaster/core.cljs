65a2baf83456eb7feb90a618682c1e5c92ab2b83
James <james@Jamess-MacBook.local>
2020-07-17 20:59:35 -0400

    app bar with no functionality

--- src/cljs/stockmaster/core.cljs
+++ src/cljs/stockmaster/core.cljs
@@ -2,7 +2,7 @@
   (:require [reagent.core :as reagent]
             [reagent.dom :as reagent-dom]
             [re-frame.core :as reframe]
-            [stockmaster.optiontable :as optiontable]
+            [stockmaster.components :as components]
             [stockmaster.events]
             [stockmaster.subscriptions]))
 
@@ -11,15 +11,11 @@
  (fn [_ _]
    {:db {}}))
 
-
-(defn app []
-  [optiontable/option-list])
-
 (defn init! []
   (reframe/dispatch-sync [:initialize-db])
   (reframe/dispatch [:request-option-expirations "SPY"])
   (reagent-dom/render
-   [app]
+   [components/app]
    (.getElementById js/document "root")))
 
 
